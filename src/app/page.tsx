0f415e8cf5ec9e7ce6e5c371a328932767b8ec92
mijanuroli <mijanworks@gmail.com>
2026-07-02 22:48:06 +0600

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { Hero, About, Skills, Projects, Experience, Education, Testimonials, Contact } from "@/components/sections";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Education />
+      <Testimonials />
+      <Contact />
+    </>
+  );
+}
